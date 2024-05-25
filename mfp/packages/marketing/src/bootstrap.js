acb9571deefbfa098e753858517270982e9ec002
Aparna venugopal <aparnavenugopal1996@gmail.com>
2024-05-25 10:07:04 +0530

    navigation done

--- mfp/packages/marketing/src/bootstrap.js
+++ mfp/packages/marketing/src/bootstrap.js
@@ -1,14 +1,29 @@
 // console.log('hi there');
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory } from 'history';
 import App from './App';
 
 
 //creation of munt function
-const mount = (el) => {
+const mount = (el, { onNavigate }) => {
+    const history = createMemoryHistory();
+    if(onNavigate){
+      history.listen(onNavigate);
+    }
+   
     ReactDOM.render(
-        <App/>,el
-    )
+        <App history={history}/>,el
+    );
+
+    return {
+      onParentNavigate({ pathname: nextPathname}){
+        const { pathname } = history.location;
+        if(pathname !== nextPathname){
+          history.push(nextPathname);
+        }
+      }
+    }
 }
 
 //if we are development and in isolation
@@ -17,7 +32,7 @@ if(process.env.NODE_ENV === 'development'){
   const devRoot = document.querySelector('#_marketing-dev-root');
   
   if(devRoot){
-    mount(devRoot);
+    mount(devRoot, {});
   }
 }
 
